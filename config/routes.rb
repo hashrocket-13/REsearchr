f961df107b6d818d0db07796b951cf06aae49638
My Name <myemail@example.com>
2014-02-13 19:39:49 -0500

    added minimal css and custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Researchr::Application.routes.draw do
-  resources :queries
+  
 
-  resources :users
+  resources :users do
+    resources :queries
+  end
 
   #resources :sessions, only: [:new, :create, :destroy]
   get "/login", to: "session#new"
   post "/session", to: "session#create"
   delete "/session", to: "session#destroy"
+  get "/about", to: "about#index"
+  get "/contact", to: "contact#index"
 
   root "welcome#index"
 end
