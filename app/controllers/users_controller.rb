39d0c2aa5ccd3c1b4dedca01690e01bcc164b91a
My Name <myemail@example.com>
2014-02-17 12:24:39 -0500

    fixed user authentication and authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :load_user, only: [:show, :edit, :update, :destroy]
- # before_action :authenticate, :authorize, only: [:edit, :update]
+  before_action :authenticate, :authorize, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -48,20 +48,19 @@ class UsersController < ApplicationController
   end
 
   def user_params
-
     params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
   end
 
-  #   def authenticate
-  #   unless logged_in?
-  #     redirect_to login_path
-  #    end
-  #   end
+    def authenticate
+    unless logged_in?
+      redirect_to login_path
+     end
+    end
 
-  # def authorize
-  #   unless current_user == @user
-  #     redirect_to login_path
-  #   end
-  #  end
+  def authorize
+    unless current_user == @user
+      redirect_to login_path
+    end
+   end
 
 end
