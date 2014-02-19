1c8b91b5101f2e34eb6434869bbc0a3205990d26
My Name <myemail@example.com>
2014-02-19 10:56:31 -0500

    all three color values are working for rentals value score

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def algo_for_quality(avg_price, avg_sqft)
     return (avg_price / avg_sqft)
   end
+
+  def color(algo_for_quality)
+    if algo_for_quality > 55
+      return "scoregreen"
+    elsif algo_for_quality <= 55 && algo_for_quality >= 50
+      return "scoreyellow"
+    else
+      return "scorered"
+    end
+  end
 end
