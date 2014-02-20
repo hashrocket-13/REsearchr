f921876b28e191dd94525efbb9df94685e4dc894
My Name <myemail@example.com>
2014-02-20 17:18:03 -0500

    fixed ZeroDivisionError on no returned retults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   def algo_for_quality(avg_price, avg_sqft)
-    return (avg_price / avg_sqft)
+    if avg_sqft == 0
+      return 0
+    else
+      return (avg_price / avg_sqft)
+    end
   end
 
   def color(algo_for_quality)
