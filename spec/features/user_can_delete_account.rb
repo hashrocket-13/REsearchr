9352f8ff919581b46cf210e847157244b200d615
My Name <myemail@example.com>
2014-03-01 16:24:17 -0500

    added rspec test for user update account

---
+++ spec/features/user_can_delete_account.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "user can delete account" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "deletes user account" do
+    
+    # Setup
+    login(user)
+    # Workflow
+
+    # Expectations
+
+  end
+
+  def login(user)
+    visit "/login"
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button "Log in!"
+  end
+
+
+end
---
+++ spec/features/user_can_update_account.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe "user can update account info" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "can update email address" do
+    
+    # Setup
+    login(user)
+    # Workflow
+    click_link "Edit My Info"
+    fill_in :user_first_name, with: "Capybara"
+    click_button "Save Changes"
+
+    # Expectations
+    expect(page).to have_content "Welcome Capybara!"
+
+
+  end
+
+  def login(user)
+    visit "/login"
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button "Log in!"
+  end
+
+
+end
