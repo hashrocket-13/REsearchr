0c92b61cd20ebd9a50142cfc497e927f0a3d6056
My Name <myemail@example.com>
2014-03-01 19:17:13 -0500

    changed sales/rentals to dropdown on form

---
+++ spec/features/user_enters_invalid_data.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "user enters invalid data" do
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "redirects you to an error page" do
+    
+    # Setup
+    login(user)
+
+    # Worlflow
+    visit new_user_query_path(user)
+    fill_in "title", with: "sales"
+    fill_in "zip", with: "10024"
+    fill_in "beds", with: "1"
+    fill_in "baths", with: "1"
+    click_button "Submit Search"
+
+    # Expectations
+
+
+  end
+
+  def login(user)
+    visit root_path
+    click_link "login"
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    click_button "Log in!"
+  end
+
+end
